434ea0aa9501d2aa2c13ccb231be3558c1a6dfa0
makoto-komaki <otokam.ikamok1020@gmail.com>
2020-06-27 11:27:36 +0000

    fix tasks_controller-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,6 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @task = current_user.tasks.build  # form_with 用
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 

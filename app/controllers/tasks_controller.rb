26e8c8d7990543881457784c275a6ba8ad6d758a
makoto-komaki <otokam.ikamok1020@gmail.com>
2020-06-27 04:57:28 +0000

    add login-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,12 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -14,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
+  @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
